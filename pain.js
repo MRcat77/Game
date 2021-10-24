3ea00a742c1642be31ea9e8c2a743e7b701da111
MRcat77 <71332009+MRcat77@users.noreply.github.com>
2021-10-25 01:21:10 +0200

    the fuck is loops????

--- pain.js
+++ pain.js
@@ -9,7 +9,22 @@ function Point()
     document.querySelector("#counter").textContent = notes;
 }
 
+let elem = document.querySelector("#beat")
+let pos = 100;
+
 function Beat()
 {
     document.querySelector("#beat").style.opacity = "1";
+    if (pos === 0)
+    {
+        document.querySelector("#beat").style.opacity = "0";
+        pos = 100;
+    }else{
+        pos--;
+        elem.style.left = pos + "%";
+    }
 }
+
+//use the left % to check when the player should click the record to get a combo
+
+
