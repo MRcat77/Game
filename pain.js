7008cc6bb81d0d7bde46abf873e47fe1fd6e3b39
MRcat77 <notthemrcat@gmail.com>
2021-10-25 14:15:27 +0200

    basic game cosept dow
    now starting work on name popup and difrent disks

--- pain.js
+++ pain.js
@@ -1,30 +1,58 @@
 let notes = localStorage['score'] || '0';
 document.querySelector("#counter").textContent = notes;
 
+
+let combo = 1;
+let limit = 5;
+
 function Point()
 {
-    notes++
+    if(combo === limit){
+        combo = 5;
+        notes = combo + notes;
+    }else if (left >= 45 && left <= 55){
+        combo = combo + 1;
+        notes = combo + notes++;
+    }else {
+        combo = 1;
+        notes++;
+    }
+
     localStorage['score'] = notes; // only strings
-    console.log(notes)
+    document.querySelector("#counter").textContent = notes;
+
+    //console.log(notes);
+    //console.log(combo);
+}
+
+function clear()
+{
+    notes = 0;
+    combo = 0;
     document.querySelector("#counter").textContent = notes;
 }
 
-let elem = document.querySelector("#beat")
-let pos = 100;
+let elem = document.getElementById("beat");
+let left = 100;
+let id = setInterval(frame, 35);
 
-function Beat()
-{
-    document.querySelector("#beat").style.opacity = "1";
-    if (pos === 0)
-    {
-        document.querySelector("#beat").style.opacity = "0";
-        pos = 100;
-    }else{
-        pos--;
-        elem.style.left = pos + "%";
+function frame() {
+    if (left === 0) {
+        elem.style.left = 100 + "%";
+        left = 100;
+        frame();
+    } else {
+        left--;
+        elem.style.left = left + "%";
     }
 }
 
-//use the left % to check when the player should click the record to get a combo
+function popup()
+{
+    document.querySelector("#blur").style.display="none";
+}
+
+
+// use the left % to check when the player should click the record to get a combo
 
 
